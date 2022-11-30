1f20a8a5c913d692d9a4c41f1ab6caa904a2beb7
Desislava <106395233+Desislava26@users.noreply.github.com>
2022-11-30 16:03:19 +0200

    Delete Table Relations directory

--- Table Relations/PeaksInRila.sql
+++
@@ -1,7 +0,0 @@
-USE [Geography]
-
-SELECT Peaks.PeakName, Peaks.Elevation, Mountains.MountainRange
-FROM     Mountains INNER JOIN
-                  Peaks ON Mountains.Id = Peaks.MountainId
-WHERE  (Mountains.MountainRange = N'Rila')
-ORDER BY Peaks.Elevation DESC
